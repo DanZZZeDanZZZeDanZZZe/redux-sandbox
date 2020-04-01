9bf3d24ff4575d2cd7bdc717e5a89aaafaebcf9c
DanZZZeDanZZZeDanZZZe <jyjyjyj3@gmail.com>
2020-04-01 13:53:40 +0300

    the project was split into files

--- src/index.js
+++ src/index.js
@@ -1,24 +1,8 @@
 import {createStore} from 'redux'
-
-const reducer = (state = 0, action) => {
-
-    switch (action.type) {
-        case 'RND': 
-            return state + action.payload
-        case 'INC': 
-            return state + 1
-        case 'DEC':
-            return state - 1
-        default: 
-            return state
-    }
-}
+import reducer from './reducer'
+import { inc, dec, rnd } from './actions'
 
 const store = createStore(reducer)
-
-const inc = () => ({type: 'INC'})
-const dec = () => ({type: 'DEC'})
-const rnd = (payload) => ({type: 'RND', payload })
 
 document
     .getElementById('inc')
@@ -48,10 +32,4 @@ const update = () => {
 }
 
 store.subscribe(update)
-/*store.subscribe(()=>{
-    console.log(store.getState())
-})
-
-store.dispatch({type: 'INC'})
-store.dispatch({type: 'INC'})*/
 
