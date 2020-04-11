58006acb482390fc69897227da3e54f5a6fbb34f
DanZZZeDanZZZeDanZZZe <jyjyjyj3@gmail.com>
2020-04-11 23:44:02 +0300

    add react counter

--- src/index.js
+++ src/index.js
@@ -1,13 +1,18 @@
 import {createStore, bindActionCreators} from 'redux'
+import ReactDOM from 'react-dom'
+import React from 'react'
+
+
 import reducer from './reducer'
 import * as actions from './actions'
+import Counter from './counter'
 
 const store = createStore(reducer)
 const { dispatch } = store
 
 const {inc, dec, rnd} =  bindActionCreators(actions, dispatch)
 
-document
+/*document
     .getElementById('inc')
     .addEventListener('click', inc)
 
@@ -20,13 +25,25 @@ document
     .addEventListener('click', () => {
         const payload = Math.floor(Math.random()*10)
         rnd(payload)
-    })
+    })*/
 
 const update = () => {
-    document
+   /* document
         .getElementById('counter')
-        .innerHTML = store.getState()
+        .innerHTML = store.getState()*/
+    ReactDOM.render(
+        <Counter
+            counter={store.getState()}
+            inc={inc}
+            dec={dec}
+            rnd={() => {
+                const payload = Math.floor(Math.random()*10)
+                rnd(payload)
+            }}
+        />, 
+        document.getElementById('root'))
 }
 
+update()
 store.subscribe(update)
 
